c23d20ffc995bc177c2625c467c0c2c6bb950021
toruuuu <ghost.toru@gmail.com>
2024-02-29 16:33:05 +0800

    111
    111

---
+++ Guess_number/app/src/main/java/com/example/guess_number/MainActivity.kt
@@ -0,0 +1,57 @@
+package com.example.guess_number
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.widget.Button
+import android.widget.EditText
+import android.widget.TextView
+import java.util.*
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+        val textView1 = findViewById<TextView>(R.id.textView1)
+        val textView2 = findViewById<TextView>(R.id.textView2)
+        val editText1 = findViewById<EditText>(R.id.editText1)
+        val button1 = findViewById<Button>(R.id.button1)
+        val button2 = findViewById<Button>(R.id.button2)
+        var number_gap:Int
+        editText1.text.clear()
+        var min:Int=1
+        var max:Int=100
+        var secret:Int = Random().nextInt(100)+1
+        button1.setOnClickListener{
+            number_gap=editText1.text.toString().toInt()-secret
+
+            when{
+                number_gap < 0 -> {
+                    textView1.text = "你猜得太小囉!"
+                    min = editText1.text.toString().toInt()
+                    textView2.text=min.toString()+"-"+max.toString()
+                }
+                number_gap > 0 -> {
+                    textView1.text = "你猜得太大囉!"
+                    max = editText1.text.toString().toInt()
+                    textView2.text=min.toString()+"-"+max.toString()
+                }
+                else -> {
+                    textView1.text="你猜對了!!"
+                    textView2.text="正確數字是"+secret.toString()
+                }
+
+            }
+            //Toast.makeText(this,editText1.text,Toast.LENGTH_SHORT).show()
+        }
+        button2.setOnClickListener {
+            secret = Random().nextInt(100)+1
+            textView2.text=secret.toString()
+            textView1.text="重新開始猜數字"
+            textView2.text="1-100"
+            editText1.text.clear()
+            min=1
+            max=100
+        }
+    }
+}
