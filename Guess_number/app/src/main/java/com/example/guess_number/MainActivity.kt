903ec02417f81d012f954bb68b48c19a368c7af1
toruuuu <ghost.toru@gmail.com>
2024-03-07 16:41:54 +0800

    20240307v1
    模擬器故障

--- Guess_number/app/src/main/java/com/example/guess_number/MainActivity.kt
+++ Guess_number/app/src/main/java/com/example/guess_number/MainActivity.kt
@@ -2,16 +2,21 @@ package com.example.guess_number
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.os.Handler
+import android.os.Looper
 import android.widget.Button
 import android.widget.EditText
 import android.widget.TextView
+import android.widget.Toast
 import java.util.*
 
 class MainActivity : AppCompatActivity() {
+    val TAG:String = MainActivity::class.java.simpleName
+    private lateinit var handler:Handler
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
-
+        handler = Handler(Looper.getMainLooper())
         val textView1 = findViewById<TextView>(R.id.textView1)
         val textView2 = findViewById<TextView>(R.id.textView2)
         val editText1 = findViewById<EditText>(R.id.editText1)
@@ -22,14 +27,17 @@ class MainActivity : AppCompatActivity() {
         var min:Int=1
         var max:Int=100
         var secret:Int = Random().nextInt(100)+1
+
         button1.setOnClickListener{
             number_gap=editText1.text.toString().toInt()-secret
 
             when{
                 number_gap < 0 -> {
-                    textView1.text = "你猜得太小囉!"
+                    textView1.setText(getString(R.string.guess_little))
+
                     min = editText1.text.toString().toInt()
-                    textView2.text=min.toString()+"-"+max.toString()
+                    textView1.text = "你猜得太小囉!"
+                    //textView2.text=min.toString()+"-"+max.toString()
                 }
                 number_gap > 0 -> {
                     textView1.text = "你猜得太大囉!"
@@ -39,6 +47,16 @@ class MainActivity : AppCompatActivity() {
                 else -> {
                     textView1.text="你猜對了!!"
                     textView2.text="正確數字是"+secret.toString()
+                    Toast.makeText(this,"6秒後重置遊戲",Toast.LENGTH_SHORT).show()
+                    handler.postDelayed({
+                        secret = Random().nextInt(100)+1
+                        textView2.text=secret.toString()
+                        textView1.text="重新開始猜數字"
+                        textView2.text="1-100"
+                        editText1.text.clear()
+                        min=1
+                        max=100
+                    },6000)
                 }
 
             }
@@ -53,5 +71,10 @@ class MainActivity : AppCompatActivity() {
             min=1
             max=100
         }
+    }
+
+    override fun onDestroy() {
+        super.onDestroy()
+        handler.removeCallbacksAndMessages(null)
     }
 }
