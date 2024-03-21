5e803987fcb25546e8219e0686aa5f3f99435c1f
Fun1206 <imfiona719@gmail.com>
2024-03-21 13:09:20 +0800

    20240321

--- Guess_number/app/src/main/java/com/example/guess_number/MainActivity.kt
+++ Guess_number/app/src/main/java/com/example/guess_number/MainActivity.kt
@@ -10,67 +10,76 @@ import android.widget.TextView
 import android.widget.Toast
 import java.util.*
 
+
 class MainActivity : AppCompatActivity() {
     val TAG:String = MainActivity::class.java.simpleName
-    private lateinit var handler:Handler
+    private lateinit var handler: Handler
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
         handler = Handler(Looper.getMainLooper())
-        val textView1 = findViewById<TextView>(R.id.textView1)
-        val textView2 = findViewById<TextView>(R.id.textView2)
-        val editText1 = findViewById<EditText>(R.id.editText1)
-        val button1 = findViewById<Button>(R.id.button1)
-        val button2 = findViewById<Button>(R.id.button2)
-        var number_gap:Int
-        editText1.text.clear()
+        val textView1 = findViewById<TextView>(R.id.textView1)//
+        val edit_num = findViewById<EditText>(R.id.edit_num)
+        val button1 = findViewById<Button>(R.id.button1)//guess
+        val button2 = findViewById<Button>(R.id.button2)//reset
+        val textView2 = findViewById<TextView>(R.id.textView2)//區間
+
+        var validate_num:Int
         var min:Int=1
         var max:Int=100
-        var secret:Int = Random().nextInt(100)+1
 
+        var secert: Int = Random().nextInt(100)+1//隨機1-100取一數
+
+        /*handler.postDelayed({
+            Toast.makeText(this,"5秒後的操作執行了",Toast.LENGTH_SHORT).show()
+        },5000)
+        */
+
+        /*button1.setOnClickListener{
+            textView1.text=name.text
+        }*/
+        edit_num.text.clear()
+        //textView1.text=secert.toString()
+        textView2.text=min.toString()+"-"+max.toString()
         button1.setOnClickListener{
-            number_gap=editText1.text.toString().toInt()-secret
+            validate_num=edit_num.text.toString().toInt()-secert
 
-            when{
-                number_gap < 0 -> {
-                    //textView1.setText(getString(R.string.guess_little))
-
-                    min = editText1.text.toString().toInt()
-                    textView1.text = "你猜得太小囉!"
-                    textView2.text=min.toString()+"-"+max.toString()
-                }
-                number_gap > 0 -> {
-                    textView1.text = "你猜得太大囉!"
-                    max = editText1.text.toString().toInt()
-                    textView2.text=min.toString()+"-"+max.toString()
-                }
-                else -> {
-                    textView1.text="你猜對了!!"
-                    textView2.text="正確數字是"+secret.toString()
-                    Toast.makeText(this,"6秒後重置遊戲",Toast.LENGTH_SHORT).show()
-                    handler.postDelayed({
-                        secret = Random().nextInt(100)+1
-                        textView2.text=secret.toString()
-                        textView1.text="重新開始猜數字"
-                        textView2.text="1-100"
-                        editText1.text.clear()
-                        min=1
-                        max=100
-                    },6000)
-                }
-
+            if(validate_num>0)
+            {
+                textView1.text="請猜小一點 !!!"
+                max=edit_num.text.toString().toInt()
+                textView2.text=min.toString()+"-"+max.toString()
             }
-            //Toast.makeText(this,editText1.text,Toast.LENGTH_SHORT).show()
+            else if(validate_num<0)
+            {
+                textView1.text="請猜大一點 !!!"
+                min=edit_num.text.toString().toInt()
+                textView2.text=min.toString()+"-"+max.toString()
+            }
+            else
+            {
+                textView1.text="恭喜猜對了 !!!"
+                Toast.makeText(this,"~ 6秒後重置遊戲 ~",Toast.LENGTH_SHORT).show()
+                handler.postDelayed({
+                    edit_num.text.clear()
+                    min=1
+                    max=100
+                    secert = Random().nextInt(100)+1//隨機1-100取一數
+                    //textView1.text=secert.toString()
+                    textView2.text=min.toString()+"-"+max.toString()
+                },6000)
+            }
+
         }
-        button2.setOnClickListener {
-            secret = Random().nextInt(100)+1
-            textView2.text=secret.toString()
-            textView1.text="重新開始猜數字"
-            textView2.text="1-100"
-            editText1.text.clear()
+        button2.setOnClickListener{
+            edit_num.text.clear()
             min=1
             max=100
+            secert = Random().nextInt(100)+1//隨機1-100取一數
+            //textView1.text=secert.toString()
+            textView2.text=min.toString()+"-"+max.toString()
         }
+
     }
 
     override fun onDestroy() {
